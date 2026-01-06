243e31e655cfe92975c332d2beffa0cb448ba9cd
Manuel Mauricio Gomez Gallo <mmaurogg@Mac-mini-de-Manuel.local>
2026-01-06 16:38:59 -0500

    aladido test para marcar habito completo

--- test/ui/habit_list_screen_test.dart
+++ test/ui/habit_list_screen_test.dart
@@ -26,4 +26,33 @@ void main() {
     expect(find.text("titulo 2"), findsOneWidget);
     expect(find.byKey(Key("habit_switch_2")), findsOneWidget);
   });
+
+  testWidgets("El habito debe cambiar a completado al hacer tap en el switch", (
+    tester,
+  ) async {
+    await tester.pumpWidget(MaterialApp(home: HabitListScreen()));
+
+    final addButton = find.byKey(Key("add_button"));
+    await tester.tap(addButton);
+    await tester.pump();
+    await tester.tap(addButton);
+    await tester.pump();
+    await tester.tap(addButton);
+    await tester.pump();
+
+    final habitSwitch1 = find.byKey(Key("habit_switch_1"));
+    final habitSwitch2 = find.byKey(Key("habit_switch_2"));
+    final habitSwitch3 = find.byKey(Key("habit_switch_3"));
+
+    expect(habitSwitch1, findsOneWidget);
+    expect(habitSwitch2, findsOneWidget);
+    expect(habitSwitch3, findsOneWidget);
+
+    await tester.tap(habitSwitch2);
+    await tester.pump();
+
+    expect(tester.widget<Switch>(habitSwitch1).value, isFalse);
+    expect(tester.widget<Switch>(habitSwitch2).value, isTrue);
+    expect(tester.widget<Switch>(habitSwitch3).value, isFalse);
+  });
 }
